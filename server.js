ea645c8ecdf92bec410aee1406f40c6c84f63c11
Falconconsume <137781459+Falconconsume@users.noreply.github.com>
2024-05-13 14:21:04 +0300

    Created the simple version of the server

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const dotenv = require("dotenv");
+const logger = require("./utills/logger")("Server");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (url === "/healthcheck" && method === "GET") {
+    res.status = 200;
+    res.end("healthcheck passed");
+    logger.info(`${method} ${url} ${res.statusCode}`);
+  } else if (url !== "/healthcheck" || method !== "GET") {
+    res.statusCode = 404;
+    res.end();
+    logger.warn(`${method} ${url} ${res.statusCode}`);
+  }
+});
+
+server.listen(process.env.PORT, () =>
+  logger.info(`The server is running on ${port}`)
+);
