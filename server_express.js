a9fa0bd91086e37b15ca2d1081981418322f86c8
Oleg <olko4455@gmail.com>
2024-05-26 17:09:06 +0300

    did the hw7

--- server_express.js
+++ server_express.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require('path')
 const { morganLogFormat, port } = require("config");
 const { route } = require("./routes/route");
+const { pageRouter } = require("./routes/pages");
 const logger = require("./utills/logger")("server Express");
 require("dotenv").config();
 
@@ -9,8 +11,16 @@ const app = express();
 
 app.use(morgan(morganLogFormat));
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'static')))
+
 app.use(express.json());
 
+app.use('/', pageRouter)
+
+// API
 app.use("/users", route);
 
 app.listen(port, () => logger.info(`The server is running on ${port}`));
